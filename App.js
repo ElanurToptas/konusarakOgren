c12a9720444492fb598bd302459ee53d48837b2d
ElanurToptas <elanur.toptass@gmail.com>
2025-11-23 21:32:34 +0300

    proje js uzantıları ayarlandı ve gerekli kütüphaneler indirildi

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View,Text} from "react-native"
+
+function App() {
+  return (
+   <View>
+    <Text style={{fontSize:33}}>Ela</Text>
+   </View>
+  )
+}
+
+export default App
